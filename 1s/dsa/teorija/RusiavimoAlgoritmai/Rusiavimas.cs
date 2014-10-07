9aa71c0d8474d8648a9e85b0b5d9ab5e16571e03
MrJonas <jonas.l.antanaitis@gmail.com>
2014-10-07 20:04:36 +0300

    Burbulo algoritmas

--- 1s/dsa/teorija/RusiavimoAlgoritmai/Rusiavimas.cs
+++ 1s/dsa/teorija/RusiavimoAlgoritmai/Rusiavimas.cs
@@ -16,10 +16,18 @@ public class Rikiavimas{
 		for (int j = 0; j < rinkinys2.Length; j++) {
 			Console.WriteLine (rinkinys2[j] + " ");
 		}
+		//Burbulo algoritmas:
+		int[]rinkinys3 = new int[5] { 7, 4, 9, 16, 42 };
+		rinkinys3 = Burbulo (rinkinys3);
+		Console.WriteLine ("Burbulo: ");
+		for (int j = 0; j < rinkinys3.Length; j++) {
+			Console.WriteLine (rinkinys3[j] + " ");
+		}
 	}
 
 //Elementarus rusiavimo algoritmai.
 	//Išrinkimo algoritmas. Selection sort.
+	//Sukeitimo operacija vienam elementui vykdoma 1 karta.
 	static int[] Isrinkimo(int[] masyvas){
 		int max, temp, id;
 		for (int i = 0; i < masyvas.Length; i++) {
@@ -39,10 +47,12 @@ public class Rikiavimas{
 	}
 
 	//Iterpimo algoritmas:
+	//Tinka, kai sukeitimas įvykdomas lengvai.
 	static int[] Iterpimo(int[] masyvas){
+		int j, temp;
 		for (int i = 0; i < masyvas.Length; i++) {
-			int j = i;
-			int temp = masyvas [i];
+			j = i;
+			temp = masyvas [i];
 			while (j < masyvas.Length - 1 && temp > masyvas[j+1]){
 				masyvas [j] = masyvas [j + 1];
 				j++;
@@ -51,9 +61,21 @@ public class Rikiavimas{
 			}
 		return masyvas;
 		}	
-	//Šelo algoritmas:
+
 	//Burbulo algoritmas:
-
-
+	static int[] Burbulo(int[] masyvas){
+		int temp;
+		for (int i = 0; i < masyvas.Length; i++){
+			for (int j = i; j + 1 < masyvas.Length; j++){
+				if (masyvas[j] > masyvas[j + 1]){
+					temp = masyvas[j + 1];
+					masyvas[j + 1] = masyvas[j];
+					masyvas[j] = temp; 
+				}
+			}
+		}
+		return masyvas;
+	}
+	//Šelo algoritmas:
 
 }
